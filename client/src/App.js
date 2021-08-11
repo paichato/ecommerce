0789ec0090a1115fcf65bc53c1b806af36a243dd
Paichato <tsenane14@gmail.com>
2021-08-11 17:31:48 +0200

    basci routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 
+import {Switch,Route} from 'react-router-dom'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
       <p>Ecommerce</p>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Switch>
     </div>
   );
 }
