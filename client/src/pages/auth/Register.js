0d0ac0e394a8c8cd6e88a39418f415d177b1e6c7
Paichato <tsenane14@gmail.com>
2021-08-12 23:36:15 +0200

    register form done

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Register() {
+
+    const [email, setEmail] = useState('');
+
+    const handleSubmit=()=>{
+
+    }
+
+    
+
+    const RegisterForm=()=>{
+        return(
+            <form onSubmit={handleSubmit}>
+                <input type='email' className='form-control' value={email} placeholder="Email" autoFocus onChange={e=>setEmail(e.target.value)} />
+                <button type='submit' className='btn btn-primary mt-3'>Confirm</button>
+                
+            </form>
+        )
+    }
+
     return (
         <div className='container p-5'>
             <div className='row'>
-                <div className='col-md-6 offset-md-3' ></div>
+                <div className='col-md-6 offset-md-3' >
+                    <h4>Register</h4>
+                    <RegisterForm/>
+                    {email && <p className='mt-3 form-text' >We will send an email to {email}</p>}
+                </div>
             </div>
 
             <p>Register</p>
