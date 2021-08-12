e42b12bcba9e36ab788d5980262cd37108d432f5
Paichato <tsenane14@gmail.com>
2021-08-12 22:52:39 +0200

    added firebase

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Register() {
     return (
-        <div>
+        <div className='container p-5'>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3' ></div>
+            </div>
+
             <p>Register</p>
         </div>
     )
