1ae1e2078b1d32f89b4f73f1ee2543d8b7774b65
Paichato <tsenane14@gmail.com>
2021-08-14 22:21:44 +0200

    prep for login page

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
- const Login=()=> {
+import {auth} from '../../firebase';
+import {toast} from 'react-toastify';
+
+
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password,setPassword]=useState('');
+    const [error,setError]=useState(false);
+    const [errorMessage,setErrorMessage]=useState('');
+
+    useEffect(() => {
+       toast.success('welcome');
+    }, [])
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        
+    
+    }
+
+    
+
+    const LoginForm=()=>{
+        return(
+            <form onSubmit={handleSubmit}>
+                <input type='email' className='form-control' value={email} placeholder="Email" autoFocus onChange={e=>setEmail(e.target.value)} />
+                <input type='password' className='form-control' value={password} placeholder="Password"  onChange={e=>setPassword(e.target.value)} />
+
+                <button type='submit' className='btn btn-primary mt-3'>Confirm</button>
+                
+            </form>
+        )
+    }
+
     return (
-        <div>
-            <p>Login</p>
+        
+       
+        <div className='container p-5'>
+            <div className='row'>
+                <div className='col-md '  >
+                <img resize='contain' width={'90%'} height={'90%'} src='../reg.svg' alt='regImage' />
+                </div>
+            
+                <div className='col-md ' >
+                    <h4>Login</h4>
+                    <LoginForm/>
+                    {error && <p className='mt-3 form-text' >{errorMessage}}</p>}
+                   
+                </div>
+            </div>
+
+           
         </div>
+       
+        
     )
 }
 
