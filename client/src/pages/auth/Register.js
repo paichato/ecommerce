034fded2101d7b6e84adfba7ddf72b39d3213889
Paichato <tsenane14@gmail.com>
2021-08-18 10:28:45 +0200

    created server

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,73 +1,92 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import {auth} from '../../firebase';
-import {toast} from 'react-toastify';
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
+function Register({ history }) {
+  const [email, setEmail] = useState("");
+  const { user } = useSelector((state) => ({ ...state }));
 
-
-function Register() {
-
-    const [email, setEmail] = useState('');
-
-    useEffect(() => {
-       toast.success('welcome');
-    }, [])
-
-    const handleSubmit=async(e)=>{
-        e.preventDefault();
-        console.log('ENV',process.env.REACT_APP_REGISTER_REDIRECT_URL);
-        const config={
-            url:process.env.REACT_APP_REGISTER_REDIRECT_URL,
-            handleCodeInApp:true
-        }
-
-        await auth.sendSignInLinkToEmail(email,config).then(()=>{
-            toast.success(`Email is sent to ${email}. Check your email`);
-            window.localStorage.setItem('emailForRegistration',email);
-            
-        }).catch((error)=>{
-            console.log(error);
-            toast.error('Something went wrong, try again!');
-        });
-
-        setEmail('');
-    
+  useEffect(() => {
+    if (user && user.token) {
+      history.push("/");
     }
+  }, [user]);
 
-    
+  useEffect(() => {
+    toast.success("welcome");
+  }, []);
 
-    const RegisterForm=()=>{
-        return(
-            <form onSubmit={handleSubmit}>
-                <input type='email' className='form-control' value={email} placeholder="Email" autoFocus onChange={e=>setEmail(e.target.value)} />
-                <button type='submit' className='btn btn-primary mt-3'>Confirm</button>
-                
-            </form>
-        )
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("ENV", process.env.REACT_APP_REGISTER_REDIRECT_URL);
+    const config = {
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+      handleCodeInApp: true,
+    };
 
+    await auth
+      .sendSignInLinkToEmail(email, config)
+      .then(() => {
+        toast.success(`Email is sent to ${email}. Check your email`);
+        window.localStorage.setItem("emailForRegistration", email);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Something went wrong, try again!");
+      });
+
+    setEmail("");
+  };
+
+  const RegisterForm = () => {
     return (
-        
-       
-        <div className='container p-5'>
-            <div className='row'>
-                <div className='col-md '  >
-                <img resize='contain' width={'90%'} height={'90%'} src='../reg.svg' alt='regImage' />
-                </div>
-            
-                <div className='col-md ' >
-                    <h4>Register</h4>
-                    <RegisterForm/>
-                    {email && <p className='mt-3 form-text' >We will send an email to {email}</p>}
-                   
-                </div>
-            </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          placeholder="Email"
+          autoFocus
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" className="btn btn-primary mt-3">
+          Confirm
+        </button>
+      </form>
+    );
+  };
 
-           
+  return (
+    <div className="container p-5">
+      {user ? (
+        <>
+          <h3>Redirecting...</h3>
+        </>
+      ) : (
+        <div className="row">
+          <div className="col-md ">
+            <img
+              resize="contain"
+              width={"90%"}
+              height={"90%"}
+              src="../reg.svg"
+              alt="regImage"
+            />
+          </div>
+
+          <div className="col-md ">
+            <h4>Register</h4>
+            <RegisterForm />
+            {email && (
+              <p className="mt-3 form-text">We will send an email to {email}</p>
+            )}
+          </div>
         </div>
-       
-        
-    )
+      )}
+    </div>
+  );
 }
 
-export default Register
+export default Register;
