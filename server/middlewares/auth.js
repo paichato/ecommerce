3c6fe1788a82f29491778741ac73adf656c3f6ca
paichato <tsenane14@gmail.com>
2022-02-23 01:08:56 +0200

    added token validator

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,21 @@
-const admin=require('../firebase');
+const admin = require("../firebase");
 
-exports.authCheck=(req,res,next)=>{
-    console.log(req.headers);
+exports.authCheck = async (req, res, next) => {
+  console.log(req.headers);
+
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    console.log("FIREBASE USER IN AUTHCHECK", firebaseUser);
+    req.user = firebaseUser;
     next();
-}
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      error: "Invalid or expired token",
+    });
+  }
+
+  next();
+};
