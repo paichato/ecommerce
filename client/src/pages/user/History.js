da7fc5df63880456679ea9f3f4d6f72d0ac68734
paichato <tsenane14@gmail.com>
2022-02-26 15:00:04 +0200

    added sidebar nav

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,9 +1,14 @@
 import React from "react";
+import UserNav from "../../components/nav/UserNav";
 
 function History() {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="colmd-2">
+          <UserNav />
+        </div>
+
         <div className="col">user history page</div>
       </div>
     </div>
