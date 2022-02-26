f5fabce20820f919d226a35d608fb414df4eb580
paichato <tsenane14@gmail.com>
2022-02-27 00:22:38 +0200

    added category read controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -13,6 +13,9 @@ exports.create = async (req, res) => {
 exports.list = async (req, res) => {
   res.json(await Category.find({}).sort({ createAt: -1 }).exec());
 };
-exports.read = async (req, res) => {};
+exports.read = async (req, res) => {
+  let category = await Category.findOne({ slug: req.params.slug }).exec();
+  res.json(category);
+};
 exports.update = async (req, res) => {};
 exports.remove = async (req, res) => {};
