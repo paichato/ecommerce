05963833930a3c2a1aab0226dd82dd2218db201f
paichato <tsenane14@gmail.com>
2022-02-26 21:52:43 +0200

    added category controllers

---
+++ server/controllers/category.js
@@ -0,0 +1,7 @@
+const Category = require("../models/category");
+
+exports.create = async (req, res) => {};
+exports.list = async (req, res) => {};
+exports.read = async (req, res) => {};
+exports.update = async (req, res) => {};
+exports.remove = async (req, res) => {};
