7b25f84c83f73a94570b9a87f663b1b7bad56f14
paichato <tsenane14@gmail.com>
2022-02-26 16:50:20 +0200

    added admin check

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   //   console.log(req.headers);
@@ -18,4 +19,17 @@ exports.authCheck = async (req, res, next) => {
   }
 
   next();
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+  const adminUser = await User.findOne({ email }).exec();
+
+  if (adminUser !== "admin") {
+    res.status(403).json({
+      err: "Admin resources. Access denied.",
+    });
+  } else {
+    next();
+  }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
-const express=require('express');
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
 //middleware
-const {authCheck}=require('../middlewares/auth');
+const { authCheck, adminCheck } = require("../middlewares/auth");
 
-const {createOrUpdateUser, currentUser}=require('../controllers/auth');
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
-router.post('/create-or-update-user',authCheck, createOrUpdateUser);
-router.post('/current-user',authCheck, currentUser);
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
-module.exports=router;
+module.exports = router;
