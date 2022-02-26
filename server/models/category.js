80a93e8e6df6937c73c9b9baddfadaa87b75c1de
paichato <tsenane14@gmail.com>
2022-02-26 21:43:45 +0200

    added categorymodel

---
+++ server/models/category.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// const { ObjectId } = mongoose.Schema;
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+      minlenght: [3, "Too short"],
+      maxlenght: [32, "Too long"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
