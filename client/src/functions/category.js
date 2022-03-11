ce62d3ed8e9df47dce09996ae205e2d0f8f7d600
paichato <tsenane14@gmail.com>
2022-03-11 23:58:03 +0200

    added client create/update categories function

--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -12,3 +12,21 @@ export const removeCategory = async (slug, authtoken) =>
       authtoken,
     },
   });
+
+export const updateCategory = async (slug, category, authtoken) =>
+  await axios.put(
+    `${process.env.REACT_APP_API}/category/${slug}`,
+    { name: category },
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+
+export const createCategory = async (category, authtoken) =>
+  await axios.post(`${process.env.REACT_APP_API}/category/`, category, {
+    headers: {
+      authtoken,
+    },
+  });
