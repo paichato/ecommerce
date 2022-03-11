a79b083615bd7e7f77f9768b0b4cdd994c5ad527
paichato <tsenane14@gmail.com>
2022-03-11 23:44:44 +0200

    added client lis categories function

---
+++ client/src/functions/category.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const getCategories = async () =>
+  await axios.get(`${process.env.REACT_APP_API}/categories`);
+
+export const getCategory = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
