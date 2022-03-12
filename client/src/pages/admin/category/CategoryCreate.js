3def3cf4be277b84ba61cbfda2ed03dcb5cd8695
paichato <tsenane14@gmail.com>
2022-03-12 02:28:51 +0200

    added category Create page ++

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import AdminNav from "../../../components/nav/AdminNav";
+import { createCategory } from "../../../functions/category";
 
 export default function CategoryCreate() {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {}, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    setLoading(true);
+    createCategory({ name }, user.token)
+      .then((res) => {
+        console.log(res.data);
+        setName("");
+        toast.success(`category ${res.data.name} has been created`);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        err.response.status === 400 && toast.error(err.response.data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const CategoryForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+            required
+          />
+          <button disabled={loading} className="btn btn-outline-primary mt-2">
+            Save
+          </button>
+        </div>
+      </form>
+    );
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -9,7 +58,10 @@ export default function CategoryCreate() {
           <AdminNav />
         </div>
 
-        <div className="col">category create page</div>
+        <div className="col">
+          <h4>Create Category</h4>
+          <CategoryForm />
+        </div>
       </div>
     </div>
   );
