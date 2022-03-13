67a917c8436451f192f41a87fef809e3956b0fd3
paichato <tsenane14@gmail.com>
2022-03-13 19:01:55 +0200

    added product route

---
+++ server/routes/product.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+//middleware
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+const {
+  create,
+  //   list,
+  //   read,
+  //   update,
+  //   remove,
+} = require("../controllers/product");
+
+router.post("/product", authCheck, adminCheck, create);
+// router.get("/categories", list);
+// router.get("/category/:slug", read);
+// router.put("/category/:slug", authCheck, adminCheck, update);
+// router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
