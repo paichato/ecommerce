c6f66ff0242ca0d8d67234bb91aa65b018870552
paichato <tsenane14@gmail.com>
2022-03-13 19:31:16 +0200

    added product Page structure

---
+++ client/src/pages/product/ProductCreate.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import AdminNav from "../../../components/nav/AdminNav";
+// import {
+//   createCategory,
+//   getCategories,
+//   removeCategory,
+// } from "../../../functions/category";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import CategoryForm from "../../../components/forms/CategoryForm";
+import LocalSearch from "../../../components/forms/LocalSearch";
+
+export default function ProductCreate() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+
+        <div className="col-md-10">Product create form</div>
+      </div>
+    </div>
+  );
+}
