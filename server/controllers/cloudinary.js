d4265f0d85f8d56ee67153b583d64271ee11a42d
paichato <tsenane14@gmail.com>
2022-04-07 20:23:29 +0200

    added cloudinary methods to upload

--- server/controllers/cloudinary.js
+++ server/controllers/cloudinary.js
@@ -3,7 +3,25 @@ const cloudinary = require("cloudinary");
 //cloudinary config
 
 cloudinary.v2.config({
-  cloud_name: procces.env.CLOUDINARY_CLOUD_NAME,
-  api_key: procces.env.CLOUDINARY_API_KEY,
-  api_secret: proccess.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+exports.upload = async (req, res) => {
+  let result = await cloudinary.v2.uploader.upload(req.body.image, {
+    public_id: `${Date.now()}`,
+    resource_type: "auto",
+  });
+  res.json({
+    public_id: result.public_id,
+    url: result.secure_url,
+  });
+};
+exports.remove = (req, res) => {
+  let image_id = req.body.public_id;
+  cloudinary.v2.uploader.destroy(image_id, (err, result) => {
+    if (err) return res.json({ success: false, err });
+    res.send("removed successfuly");
+  });
+};
