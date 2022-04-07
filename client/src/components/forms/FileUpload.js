7d207278879863f35365200651fb1a01264de0a6
paichato <tsenane14@gmail.com>
2022-04-07 21:19:10 +0200

    added logic to resize images

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,11 +1,35 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import { useSelector } from "react-redux";
 
 export default function FileUpload() {
-  const fileUploadAndResize = () => {};
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const fileUploadAndResize = (e) => {
+    console.log(e.target.files[0]);
+    let files = e.target.files;
+    console.log([files]);
+    if (files) {
+      [...files].map((img) => {
+        Resizer.imageFileResizer(
+          img,
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          "base64"
+        );
+      });
+    }
+  };
 
   return (
     <div className="row">
-      <label className="btn btn-primary">
+      <label className="btn btn-primary btn-raised">
         Choose file
         <input
           type="file"
