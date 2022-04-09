0a309987f59055e7451e0a81108ad4ff14a4dc0e
paichato <tsenane14@gmail.com>
2022-04-09 19:10:59 +0200

    added edit product function +

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -15,18 +15,46 @@ import {
 } from "@ant-design/icons";
 import CategoryForm from "../../../components/forms/CategoryForm";
 import LocalSearch from "../../../components/forms/LocalSearch";
-import { createProduct } from "../../../functions/product";
+import { createProduct, getProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import { Button, Result, Skeleton, Spin } from "antd";
 
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  category: "",
+  categories: [],
+  subs: [],
+  images: [],
+  colors: ["Silver", "Gold", "Black", "White", "Brown"],
+  brands: ["Apple", "Samsung", "Asus", "Lenovo", "Microsoft"],
+  color: "",
+  brand: "",
+  shipping: "",
+  quantity: "",
+};
+
 export default function ProductUpdate({ match }) {
   const { user } = useSelector((state) => ({ ...state }));
   const [done, setDone] = useState(false);
+  const [values, setValues] = useState(initialState);
 
   let params = useParams();
-  console.log(params.slug);
+  //   console.log(params.slug);
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then((p) => {
+      console.log("single product:", p.data);
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   return (
     <div className="container-fluid">
