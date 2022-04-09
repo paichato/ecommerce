1e889a9905705b6f4b07eabd63d3fc8e701d0d74
paichato <tsenane14@gmail.com>
2022-04-09 02:05:25 +0200

    added listall products route and controller

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,13 +8,13 @@ const { authCheck, adminCheck } = require("../middlewares/auth");
 const {
   create,
   //   list,
-  read,
+  listAll,
   //   update,
   //   remove,
 } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
-router.get("/products", read);
+router.get("/products:count", listAll);
 // router.get("/categories", list);
 // router.get("/category/:slug", read);
 // router.put("/category/:slug", authCheck, adminCheck, update);
