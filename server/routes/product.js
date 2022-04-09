aba8fb71ab8382e7031d973d712a971f3f901cae
paichato <tsenane14@gmail.com>
2022-04-09 18:55:02 +0200

    added edit product controller

--- server/routes/product.js
+++ server/routes/product.js
@@ -9,13 +9,14 @@ const {
   create,
   //   list,
   listAll,
-  //   update,
+  read,
   remove,
 } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", authCheck, read);
 // router.get("/categories", list);
 // router.get("/category/:slug", read);
 // router.put("/category/:slug", authCheck, adminCheck, update);
